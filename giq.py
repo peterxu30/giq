0453ca95518798c2e091c4dc7c3a9c1b516660bd
Peter Xu <peterxu30@users.noreply.github.com>
2016-12-22 18:20:59 -0800

    switched to Requests library for http

--- giq.py
+++ giq.py
@@ -1,4 +1,4 @@
-from urllib import request
+import requests
 import sys
 
 URL = "http://128.32.37.201:8079/api/query"
@@ -6,7 +6,7 @@ URL = "http://128.32.37.201:8079/api/query"
 query_bindings = {}
 
 def help(_):
-	return 'Commands:\nhelp: get list of commands.\nexit: exit the program\nbind: bind a query to a shortcut\nsee <shortcut>: see the query bound to the given shortcut\nlist: list all query bindings\nurl: see the current target url\nchange <new_url>: change the target url'
+	return 'Commands:\nhelp: get list of commands.\nexit: exit the program\nbind: bind a query to a shortcut\nsee <shortcut>: see the query bound to the given shortcut\nlist: list all query bindings\nurl: see the current target url\nswitch <new_url>: change the target url'
 
 def exit(_):
 	sys.exit()
@@ -15,14 +15,13 @@ def post_query(input):
 	query = input[0]
 	if query in query_bindings:
 		query = query_bindings[query]
-	req = request.Request(URL, query.encode())
-	resp = request.urlopen(req)
-	return resp
+	req = requests.post(URL, data=query.encode())
+	return req.json()
 
 def get_url(_):
 	return URL
 
-def change_url(input):
+def switch_url(input):
 	global URL
 	URL = input[0]
 
@@ -35,6 +34,9 @@ def get_binding(input):
 def get_all_bindings(input):
 	all_bindings = ""
 	length = len(query_bindings)
+	if length == 0:
+		return "no bindings"
+		
 	for k in query_bindings:
 		v = query_bindings[k]
 		all_bindings += k
@@ -49,12 +51,11 @@ def get_all_bindings(input):
 functions = {
 	"help": help,
 	"exit": exit,
-	"change": change_url,
+	"switch": switch_url,
 	"url": get_url,
 	"bind": bind,
 	"see": get_binding,
 	"list": get_all_bindings,
-	"query": post_query
 }
 
 def interactive_loop():
@@ -67,15 +68,16 @@ def interactive_loop():
 			if command in functions:
 				out = functions[command](tokens)
 			else:
-				out = functions["query"](line)
+				out = post_query(line)
 
 			if out != None:
-				print(out + "\n")
+				print(out)
+				print("")
 		except KeyboardInterrupt:
 			raise
 		except SystemExit:
 			raise
-		except:
-			print("malformed input")
+		except Exception as e:
+			print(e)
 
 interactive_loop()
