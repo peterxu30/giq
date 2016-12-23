0ea5bbda22c6b37d86b175482921cd3c933960d2
Peter Xu <peterxu30@users.noreply.github.com>
2016-12-23 11:52:25 -0800

    stores bindings across sessions

--- giq.py
+++ giq.py
@@ -1,16 +1,27 @@
+import os.path
+import pickle
 import pprint
 import requests
 import sys
 
 URL = "http://128.32.37.201:8079/api/query"
+BINDINGS_FILE = ".bindings.p"
 
 query_bindings = {}
 
+def startup():
+	global query_bindings
+	if os.path.isfile(BINDINGS_FILE):
+		query_bindings = pickle.load(open(BINDINGS_FILE, "rb"))
+
 def help(_):
-	return 'Commands:\nhelp: get list of commands.\nexit: exit the program\nbind: bind a query to a shortcut\nsee <shortcut>: see the query bound to the given shortcut\nlist: list all query bindings\nurl: see the current target url\nswitch <new_url>: change the target url'
+	return 'Commands:\nhelp: get list of commands.\nexit: exit the program\nbind <shortcut> <query>: bind a query to a shortcut\nunbind <shortcut>: unbind a shortcut from a query\nsee <shortcut>: see the query bound to the given shortcut\nlist: list all query bindings\nclear: unbind all query bindings\nurl: see the current target url\nswitch <new_url>: change the target url'
 
 def exit(_):
 	sys.exit()
+
+def save_bindings():
+	pickle.dump(query_bindings, open(BINDINGS_FILE, "wb"))
 
 def post_query(input):
 	query = input[0]
@@ -28,6 +39,9 @@ def switch_url(input):
 
 def bind(input):
 	query_bindings[input[0]] = input[1]
+
+def unbind(input):
+	del query_bindings[input[0]]
 
 def get_binding(input):
 	return query_bindings[input[0]]
@@ -49,14 +63,19 @@ def get_all_bindings(input):
 		length -= 1
 	return all_bindings
 
+def clear(input):
+	query_bindings.clear()
+
 functions = {
 	"help": help,
 	"exit": exit,
 	"switch": switch_url,
 	"url": get_url,
 	"bind": bind,
+	"unbind": unbind,
 	"see": get_binding,
 	"list": get_all_bindings,
+	"clear": clear
 }
 
 def interactive_loop():
@@ -75,10 +94,13 @@ def interactive_loop():
 				print(out)
 				print("")
 		except KeyboardInterrupt:
+			save_bindings()
 			raise
 		except SystemExit:
+			save_bindings()
 			raise
 		except Exception as e:
 			print(e)
 
+startup()
 interactive_loop()
